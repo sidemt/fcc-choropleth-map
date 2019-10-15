08318965aa7ec1284378f66b1395a2979e891436
sidemt <25644062+sidemt@users.noreply.github.com>
2019-10-15 16:35:22 +0900

    Create a function to retrieve the data from the API

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,34 @@
-d3.select("body")
-  .append("p")
-  .text("Hello world");
+// US Education Data
+const dataUrl =
+  'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json';
+
+// US County Data
+// const countyDataUrl =
+//   'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';
+
+getDataset();
+
+// Retrieve the dataset
+function getDataset() {
+  // Instanciate XMLHttpRequest Object
+  req = new XMLHttpRequest();
+  // Initialize GET request
+  req.open('GET', dataUrl, true);
+  // Send the request
+  req.send();
+  // onload event handler
+  req.onload = function() {
+    // Parse the returned JSON string to JavaScript object
+    json = JSON.parse(req.responseText);
+    // use the value of "data" only
+    const dataset = json;
+    drawChart(dataset);
+  };
+};
+
+// Draw chart
+function drawChart(dataset) {
+  d3.select('#graph')
+      .append('p')
+      .text(JSON.stringify(dataset));
+}
